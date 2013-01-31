a08b8005ed3baf8d4b2d3fc857026c00a2037703
btrinh1 <btrinh@ualberta.ca>
2013-01-31 14:31:05 -0700

    Update Test/src/test.java
    Added new print and stub

--- Test/src/test.java
+++ Test/src/test.java
@@ -1,12 +1,19 @@
 
-
 public class test
 {
 
     public static void main (String[] args){
         
         System.out.println("Hello");
+        System.out.println("new print!");
         
+        
+        stub();
+    }
+    
+    public void stub(){
+        
+        // stub
         
     }
     
